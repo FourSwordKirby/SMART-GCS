97cec9f491dc9f059c9514c7b45c9e6ef86bb0dc
Roger Liu <rogliu26@gmail.com>
2015-02-13 20:43:07 -0500

    added my changes to the headquarters

---
+++ SMART/Assets/SMART Assets/Scripts/HeadquarterParameters.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadquarterParameters : MonoBehaviour {
+
+    private int health;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        health = 30;
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health != 30)
+        {
+            Debug.Log("I died");
+        }
+	}
+
+    void takeDamage(int damage)
+    {
+        this.health -= damage;
+    }
+}
