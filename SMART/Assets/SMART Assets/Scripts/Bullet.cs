29b74f19e2a10ed46e32bffb57e8d1b73373ec1e
Mark Mendell <m.e.mendell@gmail.com>
2015-02-13 20:38:40 -0500

    Bullet and BigExplosion scripts & prefabs; includes required Standard Assets

---
+++ SMART/Assets/SMART Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿// 2/6/2015 - Mark Mendell
+
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	
+	public int duration = 5; // seconds
+	public int unitsPerSecond = 10;
+	public ParticleSystem explosionSystem;
+	private GameObject explosion;
+	
+	void Start () {
+		// Destroy the object after <duration> seconds
+		Destroy (gameObject, duration);
+		Debug.Log ("Bullet instantiated.");
+	}
+
+	void Update () {
+		// Move forward
+		transform.Translate (Vector3.forward * Time.deltaTime * unitsPerSecond);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log ("Collided with something.");
+		this.spawnExplosion ();
+		Destroy (gameObject);
+	}
+
+	void spawnExplosion() {
+		ParticleSystem newExplosion = Instantiate (explosionSystem) as ParticleSystem;
+		newExplosion.transform.position = gameObject.transform.position;
+		newExplosion.Play ();
+		Debug.Log ("Spawned an explosion.");
+	}
+}
---
+++ SMART/Assets/SMART Assets/Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿// 2/13/2015 - Mark Mendell
+
+using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	private ParticleSystem ps;
+	
+	void Start () {
+		ps = GetComponent<ParticleSystem> ();
+	}
+
+	void Update () {
+		// Check if done exploding
+		if (!ps.IsAlive ())
+			Destroy (gameObject);
+	}
+}
