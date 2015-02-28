281d51ce866fcb6247629d572f45f86e21c804f8
Mark Mendell <m.e.mendell@gmail.com>
2015-02-27 19:32:39 -0500

    Added basic health script.

---
+++ SMART/Assets/SMART Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿// 2/27/2015 - Mark Mendell (aka Speed McGee)
+
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	
+	private int health;
+
+	public int getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public void subtractHealth(int damage) {
+		this.health -= damage;
+	}
+
+	public void addHealth(int health) {
+		this.health += health;
+	}
+
+	void Update () {
+		if (this.health <= 0)
+			Destroy (gameObject);
+	}
+}
