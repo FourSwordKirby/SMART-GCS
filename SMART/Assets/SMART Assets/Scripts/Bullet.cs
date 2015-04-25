3b606ec961f3ff5a7f19aef4229c0446b554dfd4
BarryLiCMU <barryl@andrew.cmu.edu>
2015-04-25 15:02:31 -0400

    Bullet Fix
    Fixed Missiles and Bullet Trails

--- SMART/Assets/SMART Assets/Scripts/Bullet.cs
+++ SMART/Assets/SMART Assets/Scripts/Bullet.cs
@@ -9,24 +9,25 @@ public class Bullet : MonoBehaviour {
 	public int unitsPerSecond = 10;
 	public int damage = 1;
 	public ParticleSystem explosionSystem;
+	/*
 	public ParticleSystem missileTrail;
+	public bool useTrail;
+	*/
 	private int frameCounter = 0;
 
 	void Start () {
 		// Destroy the object after <duration> seconds
 		Destroy (gameObject, duration);
+		//missileTrail = null;
 	//	Debug.Log ("Bullet instantiated.");
-		if (missileTrail != null) {
-			this.spawnTrail ();
-		}
 	}
 
 	void Update () {
 		// Move forward
 		transform.Translate (Vector3.forward * Time.deltaTime * unitsPerSecond);
-		if (frameCounter % 2 == 0) {
+		/*if (useTrail && missileTrail != null && frameCounter % 2 == 0) {
 			this.spawnTrail ();
-		}
+		}*/
 		frameCounter++;
 	}
 
@@ -47,10 +48,12 @@ public class Bullet : MonoBehaviour {
 		newExplosion.transform.rotation = Quaternion.identity;
 		newExplosion.Play ();
 	}
+	/*
 	void spawnTrail() {
 		ParticleSystem newExplosion = Instantiate (missileTrail) as ParticleSystem;
 		newExplosion.transform.position = gameObject.transform.position;
 		newExplosion.transform.rotation = Quaternion.identity;
 		newExplosion.Play ();
 	}
+	*/
 }
